244fae2700ba393e73c71763b5a05e57cda7e061
ValkSam <do110473sva@gmail.com>
2020-03-31 21:51:11 +0300

    HW-6

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.contact import Contact
-from fixture.application_contact import Application_contact
-
-@pytest.fixture
-def app(request):
-    fixture = Application_contact()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_new_contact(app):
