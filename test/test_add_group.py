5e134a8c28852e0b06d1395c80d321f07afcd64c
ValkSam <do110473sva@gmail.com>
2020-07-17 16:14:22 +0300

    HW29

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-
+import pytest
 
 def test_add_new_group(app, db, json_groups, check_ui):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
-
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-    if check_ui:
-         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with pytest.allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with pytest.allure.step('Add a group %s to the list' % group):
+        app.group.create(group)
+    with pytest.allure.step('New group list is equal to tte old list with added group'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 # def test_add_new_empty_group (app):
 # old_groups = app.group.get_group_list()
